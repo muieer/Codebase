9a08c4bc97ffa535a835ca57a9b02cf1408e9930
muieer <yang_for_work@qq.com>
2023-10-15 14:19:17 +0800

    solve leetcode 392

--- src/main/scala/org/muieer/scala/leetcode/LeetCode392.scala
+++ src/main/scala/org/muieer/scala/leetcode/LeetCode392.scala
@@ -1,16 +1,34 @@
 package org.muieer.scala.leetcode
 
+import scala.util.control.Breaks.break
+
 object LeetCode392 {
 
-//  def isSubsequence(s: String, t: String): Boolean = {
-//
-//
-//    for {
-//      i <- t.indices
-//    } {
-//
-//    }
-//  }
+  def main(args: Array[String]): Unit = {
+    println(isSubsequence("aaaaaa", "bbaaaa"))
+  }
+
+  def isSubsequence(s: String, t: String): Boolean = {
+    if (s.isEmpty) return true
+    if (t.isEmpty) return false
+
+    var si = 0
+    var ti = 0
+
+    while (si < s.length && ti < t.length) {
+      // 依次匹配每个待匹配的字符
+      while (ti < t.length && t(ti) != s(si)) {
+        ti = ti + 1
+      }
+      // 匹配成功一个字符
+      if (ti != t.length) {
+        si = si + 1 // 指针右移
+        ti = ti + 1 // 指针右移
+      }
+    }
+
+    si == s.length
+  }
 
 
 }
