0b82b771ddfb06f54df1b52928307c6d64d8e612
muieer <yang_for_work@qq.com>
2023-11-01 20:29:16 +0800

    IOUtilsExample add inputStreamToByteArrayOutputStream(String path) method

--- src/main/java/org/muieer/java/io/IOUtilsExample.java
+++ src/main/java/org/muieer/java/io/IOUtilsExample.java
@@ -1,6 +1,11 @@
 package org.muieer.java.io;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -16,12 +21,24 @@ public class IOUtilsExample {
     * 原因：创建一个文件输入流，JVM 会在 OS 内核创建文件句柄，进行 rwx 操作，句柄资源是有限的，需要关闭流回收资源
     * */
 
-    public static final int EOF = -1;
+    static final int EOF = -1;
+    static FileSystem fs;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        StringToInputStream("hello");
+        fs = FileSystem.get(new Configuration());
+        inputStreamToByteArrayOutputStream(args[0]);
 
+    }
+
+    public static void inputStreamToByteArrayOutputStream(String path) throws Exception {
+        try (
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                FSDataInputStream inputStream = fs.open(new Path(path));
+        ) {
+            IOUtils.copyLarge(inputStream, outputStream, new byte[1024]);
+            System.out.println(outputStream.toByteArray().length);
+        }
     }
 
     public static void StringToInputStream(String str) {
