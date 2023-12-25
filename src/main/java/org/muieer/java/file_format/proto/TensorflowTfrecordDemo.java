52281035787b3adaaf383eeebca7eb3f38b094ba
muieer <yang_for_work@qq.com>
2023-12-25 22:19:44 +0800

    temp

---
+++ src/main/java/org/muieer/java/file_format/proto/TensorflowTfrecordDemo.java
@@ -0,0 +1,13 @@
+package org.muieer.java.file_format.proto;
+
+// https://www.tensorflow.org/tutorials/load_data/tfrecord
+public class TensorflowTfrecordDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void writeExampleToFile() {
+
+    }
+}
